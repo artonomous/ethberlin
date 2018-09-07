e8f1a0528bc1f02576b20bb5a0b82a3456ac197a
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-09-08 01:34:30 +0200

    React router integration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
+import Generators from './Generators';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/generators" component={Generators} />
+    </div>
+  </Router>,
+  document.getElementById('root'));
