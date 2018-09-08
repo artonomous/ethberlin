5134c0af20e8a076ec5b9c8025250cd0cd1a77f4
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-09-08 03:33:37 +0200

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Blockies from 'react-blockies';
 import './Header.css';
 
 class Header extends React.Component {
@@ -6,15 +7,28 @@ class Header extends React.Component {
     return (
         <div className='header'>
           <div className="left">
-            <span>Here could be the account picture</span>
-            <span className="address">30x054188d35...e2a4a15913504ecfc15a9</span>
-            <span className="network">Network: Mainnet</span>
+            <div className="blockies-container">
+              <div className="blockies">
+                <Blockies
+                  seed="Address goes here"
+                  size={10}
+                  scale={3}
+                  color="#dfe"
+                  bgColor="#ffe"
+                  spotColor="#abc"
+                />
+              </div>
+              <div className="network-info">
+                <span className="address">30x054188d35...e2a4a15913504ecfc15a9</span>
+                <span className="network">Network: Mainnet</span>
+              </div>
+            </div>
           </div>
           <div className="right">
             <span className="ether">0.2 ETH</span>
-            <button className="buy-ether">Purchase ETH</button>
+            <span className="button buy-ether">Purchase ETH</span>
             <span className="soul">0.2 SOUL</span>
-            <button className="buy-soul">Purchase SOUL</button>
+            <span className="button buy-soul">Purchase SOUL</span>
           </div>
         </div>
     );
