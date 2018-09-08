2c3475f7829c5360fbbaf444f95cb3c2e3e92561
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-09-08 13:37:26 +0200

    Styling historical pieces

--- src/App.js
+++ src/App.js
@@ -20,15 +20,28 @@ class App extends Component {
             <div className="button bid-button background-color-soul">Bid</div>
           </div>
         </div>
+        <h1>Historical Pieces</h1>
         <div className="historical-pieces">
           <div className="box1">
             <img src="https://images.unsplash.com/photo-1536314360972-f52b0947329e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f6dd135053c008197d7ed1943586f88f&auto=format&fit=crop&w=1650&q=80" />
+            <div className="historical-info">
+            <p className="sold-for">30 ETH</p> 
+            <p className="generator">Awesome Generator</p> 
+            </div>
           </div>
           <div className="box2">
             <img src="https://images.unsplash.com/photo-1536314360972-f52b0947329e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f6dd135053c008197d7ed1943586f88f&auto=format&fit=crop&w=1650&q=80" />
+            <div className="historical-info">
+              <p className="sold-for">30 ETH</p> 
+              <p className="generator">Awesome Generator</p> 
+            </div>
           </div>
           <div className="box3">
             <img src="https://images.unsplash.com/photo-1536314360972-f52b0947329e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f6dd135053c008197d7ed1943586f88f&auto=format&fit=crop&w=1650&q=80" />
+            <div className="historical-info">
+              <p className="sold-for">30 ETH</p> 
+              <p className="generator">Awesome Generator</p> 
+            </div>
           </div>
         </div>
       </div>
