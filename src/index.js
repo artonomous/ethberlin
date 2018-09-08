d4d62903a190feea987a1b6ad774e9c016352291
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-09-08 02:13:38 +0200

    header

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import './index.css';
 import App from './App';
 import Generators from './Generators';
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route exact path="/generators" component={Generators} />
-    </div>
-  </Router>,
+  <div>
+    <Header />
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/generators" component={Generators} />
+      </div>
+    </Router>
+  </div>,
   document.getElementById('root'));
