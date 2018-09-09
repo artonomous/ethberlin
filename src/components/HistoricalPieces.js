fb09039cda905dffdc32c731d3d341d744417a2f
Iain Nash <iain@hioscar.com>
2018-09-09 11:02:58 +0200

    updates to readme

--- src/components/HistoricalPieces.js
+++ src/components/HistoricalPieces.js
@@ -1,11 +1,12 @@
 import React from "react";
+import RenderArtModal from "./RenderArtModal";
 
 class HistoricalPieces extends React.Component {
   render() {
     return (
       <div className="historical-pieces">
         <div className="box1">
-          <img src="https://images.unsplash.com/photo-1536314360972-f52b0947329e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f6dd135053c008197d7ed1943586f88f&auto=format&fit=crop&w=1650&q=80" />
+          <RenderArtModal url="QmX3jVktEkivu43kod5pGib3BL7ZBcCSL2CwrgN3JNnrfV/upload.p5js" />
           <div className="historical-info-wrapper">
             <div className="historical-info">
               <p className="sold-for">10 ETH</p>
@@ -14,8 +15,9 @@ class HistoricalPieces extends React.Component {
             <div className="button no-border-button">[details]</div>
           </div>
         </div>
+
         <div className="box2">
-          <img src="https://images.unsplash.com/photo-1536314360972-f52b0947329e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f6dd135053c008197d7ed1943586f88f&auto=format&fit=crop&w=1650&q=80" />
+          <RenderArtModal url="QmPUZcu9KzdSmew2hmBKwxDj7B1HbkWGUyoDYCy4PpxkKy/upload.p5js" />
           <div className="historical-info-wrapper">
             <div className="historical-info">
               <p className="sold-for">20 ETH</p>
@@ -24,8 +26,9 @@ class HistoricalPieces extends React.Component {
             <div className="button no-border-button">[details]</div>
           </div>
         </div>
+
         <div className="box3">
-          <img src="https://images.unsplash.com/photo-1536314360972-f52b0947329e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f6dd135053c008197d7ed1943586f88f&auto=format&fit=crop&w=1650&q=80" />
+          <RenderArtModal url="QmSJHBgXiEqCndjfkeJCY2EhuTsLG1T9Ai88ReB8dXQavx/upload.p5js" />
           <div className="historical-info-wrapper">
             <div className="historical-info">
               <p className="sold-for">2 ETH</p>
